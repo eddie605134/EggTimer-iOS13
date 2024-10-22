e1631e1fa6dff2e340f926a8719255b36150eace
eddie <eddie605134@gmailcom>
2024-10-22 16:25:59 +0800

    [feat] 完成timer + progress bar練習

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+	
+	
+	@IBOutlet weak var progressBar: UIProgressView!
+	@IBOutlet weak var titleLabel: UILabel!
+	
+	let eggTimes = [
+		"Soft": 5,
+		"Medium": 7,
+		"Hard": 12
+	]
+	var timer: Timer?
+	var secondRemaining: Int = 0
+	var hardness: String?
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		progressBar.progress = 0
+		
+	}
+	
+	
+	@IBAction func hardnessSelected(_ sender: UIButton) {
+		hardness = sender.currentTitle ?? "Soft"
+		resetProgressBar()
+		
+		secondRemaining = 0
+		titleLabel.text = "Boiling..."
+		triggerCountdownTimer(hardness!)
+	}
+	
+	func triggerCountdownTimer(_ hardness: String) {
+		timer?.invalidate()
 
-
+		timer = Timer.scheduledTimer(
+			timeInterval: 1.0,
+			target: self,
+			selector: #selector(updateCountdownTimer),
+			userInfo: nil,
+			repeats: true)
+	}
+	
+	@objc func updateCountdownTimer() {
+		
+		print(secondRemaining)
+		secondRemaining += 1
+		updateProgressBar(hardnessType: hardness!)
+		
+		if secondRemaining >= eggTimes[hardness!]! {
+			timer?.invalidate()
+			timer = nil
+			titleLabel.text = "Done!"
+		}
+	}
+	
+	func resetProgressBar () {
+		progressBar.progress = 0
+		titleLabel.text = "How do you like your eggs?"
+	}
+	
+	func updateProgressBar (hardnessType: String) {
+		progressBar.progress = Float(secondRemaining) / Float(eggTimes[hardnessType]!)
+	}
 }
